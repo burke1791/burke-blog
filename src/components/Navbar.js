168771402ed45e89e9da6e23effb1d5897bd8456
Chris Burke <burke1791@gmail.com>
2021-12-25 08:15:55 -0600

    changed logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src='../img/logo.svg' alt="Burkcules" style={{ width: "90px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
