2d0acabd11caa590bbfb2e0c8bb9086ac16f57d5
Chris Burke <burke1791@gmail.com>
2021-12-25 08:35:55 -0600

    Updates

---
+++ admin/cms.7a62f00c9444444ed260.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdategatsby_starter_netlify_cms"]("cms",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "620b91ecef4c365a5e88"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=cms.7a62f00c9444444ed260.hot-update.js.map
---
+++ admin/cms.94608ced4caf3487851d.hot-update.js
@@ -0,0 +1,32 @@
+"use strict";
+self["webpackHotUpdategatsby_starter_netlify_cms"]("cms",{
+
+/***/ "./src/components/all.sass":
+/*!*********************************!*\
+  !*** ./src/components/all.sass ***!
+  \*********************************/
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+    if(true) {
+      // 1640442812373
+      var cssReload = __webpack_require__(/*! ./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.id, {"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "7a62f00c9444444ed260"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=cms.94608ced4caf3487851d.hot-update.js.map
