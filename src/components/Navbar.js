2faadf64066ac1a436d8343df0a200a210ebd3b8
Chris Burke <burke1791@gmail.com>
2021-12-25 08:23:45 -0600

    changed about to projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,8 +63,8 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+              <Link className="navbar-item" to="/projects">
+                Projects
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
